41c0c6082477133c012e58aec4d2f0b8c5abaaf2
wothag <d.r.c.hagnere@gmail.com>
2020-04-01 00:39:39 +0200

    MVC created

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.login = function(){
+
+
+}
+
+exports.logout = function(){
+
+
+}
+
+exports.register = function(req , res){
+    res.send("Merci pour essayer de vous enrgistrer")
+
+
+}
+
+exports.home = function(req , res){
+    res.render('home-guest')
+}
